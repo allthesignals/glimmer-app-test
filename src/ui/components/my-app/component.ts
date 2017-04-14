8ad46e7b96aa1836fd5a32a054e244d685645744
allthesignals <wilburnforce@gmail.com>
2017-04-13 20:29:04 -0400

    Working version of fruit list app

--- src/ui/components/my-app/component.ts
+++ src/ui/components/my-app/component.ts
@@ -1,5 +1,23 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class MyApp extends Component {
+  @tracked fruits = ['oranges', 'apples', 'grapes', 'peaches'];
+  @tracked newFruit;
 
+  alertThePress() {
+    alert('alert alert alert!');
+  }
+
+  @tracked('fruits')
+  get currentFruits() {
+    return this.fruits;
+  }
+
+  addSomeFruit() {
+    this.fruits = this.fruits.concat([this.newFruit]);
+  }
+
+  updateFruit(e) {
+    this.newFruit = e.target.value;
+  }
 }
